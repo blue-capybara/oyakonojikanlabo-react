39250b3c210b26d8c0c3b75d2cef095d446095e6
blue-capybara <bluecapy5150@gmail.com>
2025-06-29 21:12:48 +0900

    Fix scroll position on article navigation

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Home } from '@/pages/Home';
 import { Events } from '@/pages/Events';
 import { EventDetail } from '@/pages/EventDetail';
@@ -12,9 +12,21 @@ import { YogaCourse } from '@/pages/YogaCourse';
 import { CultureSchool } from '@/pages/CultureSchool';
 import { PicoTop } from '@/pages/PicoTop';
 
+// ScrollToTop component to reset scroll position on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
